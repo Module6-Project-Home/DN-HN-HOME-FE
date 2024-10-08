4352eb74f7f2350db54823a38e7913739033e1e0
cmtin1807 <cmtin1807@gmail.com>
2024-10-08 16:14:41 +0700

    dang us post

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PropertyList from './components/property/PropertyList';
 import PropertyDetail from './components/property/PropertyDetail';
 import PostForm from './components/property/PostForm';
-import Header from "./components/property/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from "./components/property/Footer";
 import './App.css';
 const App = () => {
     return (
         <Router>
-            <div className="app-container">
-                <Header />
-                <Routes>
+            <div className="app-container"><Routes>
                     <Route path="/home" element={<PropertyList />} />
                     <Route path="/properties/:id" element={<PropertyDetail />} />
                     <Route path="/post" element={<PostForm />} />
                 </Routes>
-                <Footer></Footer>
             </div>
         </Router>
     );
